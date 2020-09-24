23983561d2a44107faba50863816e4852eb4a442
Shashank Kumar <kumar93.shashank@gmail.com>
2020-09-24 19:07:17 +0530

    Initial commit

---
+++ inventory-form/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+import Demo from './Inventory-Form';
+
+function App() {
+  return (
+    <Demo/>
+  );
+}
+
+export default App;
